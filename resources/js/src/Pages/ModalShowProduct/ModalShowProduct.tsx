e0e4cc5ffe5581a692be7775ced13054cf50b464
godlikeAlex <godlikedesigner@gmail.com>
2025-06-30 00:56:52 +0500

    fix: add styles for p

--- resources/js/src/Pages/ModalShowProduct/ModalShowProduct.tsx
+++ resources/js/src/Pages/ModalShowProduct/ModalShowProduct.tsx
@@ -113,7 +113,7 @@ function ModalShowProduct({
                             </li>
                             <li>
                               <i className="icon-truck"></i>Free delivery for
-                              order over $200xs
+                              order over $200
                             </li>
                           </ul>
                           <div className="no-gutters row">
