33dc9e2a843c1f22fa7c9a141578ef9946331b6d
godlikeAlex <godlikedesigner@gmail.com>
2026-03-24 23:31:22 +0500

    fix: header logo

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
 <td class="header">
 <a href="{{ $url }}" style="display: inline-block;">
-@if (trim($slot) === 'Laravel')
-<img src="https://laravel.com/img/notification-logo.png" class="logo" alt="Laravel Logo">
-@else
-{{ $slot }}
-@endif
+<img src="https://signs7.com/img/logo.png" class="logo" alt="Signs7 Logo">
 </a>
 </td>
 </tr>
